07cc5aa064717bd692a966918d9dcff698c6546f
bgfxc4 <bindermarc@gmx.de>
2022-02-22 23:39:20 +0000

    added steering and second motor

--- main.ts
+++ main.ts
@@ -5,6 +5,9 @@ import * as pwm from 'raspi-soft-pwm'
 var IN1A:pwm.SoftPWM
 var IN2A:pwm.SoftPWM
 
+var IN1B:pwm.SoftPWM
+var IN2B:pwm.SoftPWM
+
 var direction = "n"
 var speed = 0
 
@@ -14,8 +17,11 @@ wss.on("listening", () => {
 })
 raspi.init(() => {
 	console.log("[raspi] init finished")	
-	IN1A = new pwm.SoftPWM({pin: 'GPIO27', frequency: 2000})
-	IN2A = new pwm.SoftPWM({pin: 'GPIO22', frequency: 2000})
+	IN1A = new pwm.SoftPWM({pin: 'GPIO23', frequency: 2000})
+	IN2A = new pwm.SoftPWM({pin: 'GPIO24', frequency: 2000})
+	
+	IN1B = new pwm.SoftPWM({pin: 'GPIO27', frequency: 2000})
+	IN2B = new pwm.SoftPWM({pin: 'GPIO22', frequency: 2000})
 })
 
 wss.on('connection', (ws, req) => {
@@ -48,12 +54,28 @@ function handle_message(msg: string) {
 }
 
 function update_pwm_pins() {
-	console.log(speed, direction)
-	if (speed > 0) {
-		IN1A.write(speed)
-		IN2A.write(0)
-	} else {	
+	var r_speed = speed
+	var l_speed = speed
+
+	if (direction == "r") {
+		r_speed *= 0.5
+	} else if (direction == "l") {
+		l_speed *= 0.5
+	}
+
+	if (r_speed > 0) {
 		IN1A.write(0)
-		IN2A.write(Math.abs(speed))
+		IN2A.write(r_speed)
+	} else {	
+		IN1A.write(Math.abs(r_speed))
+		IN2A.write(0)
+	}
+
+	if (l_speed > 0) {
+		IN1B.write(0)
+		IN2B.write(l_speed)
+	} else {	
+		IN1B.write(Math.abs(l_speed))
+		IN2B.write(0)
 	}
 }
