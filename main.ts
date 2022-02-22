d130fa2eec40c8340ce70129dd5244b4f4b74211
bgfxc4 <bindermarc@gmx.de>
2022-02-22 20:17:48 +0000

    added basic pwm control and updated dependencies

--- main.ts
+++ main.ts
@@ -2,11 +2,21 @@ import * as WebSocket from "ws"
 import * as raspi from 'raspi'
 import * as pwm from 'raspi-soft-pwm'
 
+var IN1A:pwm.SoftPWM
+var IN2A:pwm.SoftPWM
+
+var direction = "n"
+var speed = 0
+
 const wss = new WebSocket.Server({ port: 5555 })
 wss.on("listening", () => {
 	console.log("[ws] server up")
 })
-raspi.init(() => console.log("[raspi] init finished"))
+raspi.init(() => {
+	console.log("[raspi] init finished")	
+	IN1A = new pwm.SoftPWM({pin: 'GPIO27', frequency: 2000})
+	IN2A = new pwm.SoftPWM({pin: 'GPIO22', frequency: 2000})
+})
 
 wss.on('connection', (ws, req) => {
 	ws.on('message', data => {
@@ -21,12 +31,29 @@ function handle_message(msg: string) {
 	var command = msg.slice(0, msg.indexOf(" "))
 	switch (command) {
 		case "direction":
-			console.log("direction: %s", content)
+			if (["n", "l", "r"].includes(content)) {
+				direction = content
+				update_pwm_pins()
+			}
 			break
 		case "speed":
-			console.log("speed: %s", content)
+			if (parseFloat(content) >= -1 && parseFloat(content) <= 1) {
+				speed = parseFloat(content)
+				update_pwm_pins()
+			}
 			break
 		default:
 			console.log("wrong command")
+	}
+}
+
+function update_pwm_pins() {
+	console.log(speed, direction)
+	if (speed > 0) {
+		IN1A.write(speed)
+		IN2A.write(0)
+	} else {	
+		IN1A.write(0)
+		IN2A.write(Math.abs(speed))
 	}
 }
